4e9d2b3b40c2b0585f3959d7380f11c2d677b0d6
inessev <123724101+inessev@users.noreply.github.com>
2023-04-22 10:42:07 +0200

    prueba

--- java.js
+++ java.js
@@ -6,4 +6,29 @@ function pedirNumero() {
   const numero = prompt('Introduce un número entre 1 y 15');
   return parseInt(numero);
 }
-
+function comprobarNumero(numero) {
+    intentos++;
+  
+    if (numero === numeroAdivinar) {
+      console.log(`¡Felicidades! Has acertado el número en ${intentos} intentos.`);
+      return true;
+    } else if (numero < numeroAdivinar) {
+      console.log('El número que has introducido es menor.');
+    } else {
+      console.log('El número que has introducido es mayor.');
+    }
+  
+    return false;
+  }
+  
+  for (let i = 0; i < 3; i++) {
+    const numero = pedirNumero();
+  
+    if (comprobarNumero(numero)) {
+      break;
+    }
+  }
+  
+  if (intentos === 3) {
+    console.log(`Lo siento, has agotado tus intentos. El número era ${numeroAdivinar}.`);
+  }
