035d12bc42f5b5f30cb5cbb60b2a1b7cd6885b40
inessev <123724101+inessev@users.noreply.github.com>
2023-04-25 12:10:23 +0200

    prueba 3

--- java.js
+++ java.js
@@ -17,9 +17,7 @@ function comprobarNumero(numero) {
     alert('El número que has introducido es menor.');
   } else {
     alert('El número que has introducido es mayor.');
-  } if (intentos === 3) {
-    alert(`Has agotado tus intentos, el número era ${numeroAdivinar}.`);
-  }
+  } 
 
   return false;
 }
@@ -30,4 +28,7 @@ for (let i = 0; i < 3; i++) {
   if (comprobarNumero(numero)) {
     break;
   }
+}
+if (intentos === 3) {
+  alert(`Has agotado tus intentos, el número era ${numeroAdivinar}.`);
 }
